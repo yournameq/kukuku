7799c50815e9c6c340ccc3040c8275206690f53b
wen chengdong <1160159021@qq.com>
2017-08-20 22:55:32 +0800

    tijiao

--- ssss/src/ssss/hello.java
+++ ssss/src/ssss/hello.java
@@ -2,6 +2,7 @@ package ssss;
 
 public class hello {
 public static void main(String[] args) {
-	System.out.println("hello world");
+	System.out.println("hello worldsssssssssssssssssssss");
+	
 }
 }
